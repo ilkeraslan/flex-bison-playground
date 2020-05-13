b5cb413276043d37838a274154fbea7262962898
Ilker Aslan <ilkeer.ozgur@gmail.com>
2020-05-13 23:02:42 +0200

    Update HighOrderFunctions.ml

--- ML/HighOrderFunctions.ml
+++ ML/HighOrderFunctions.ml
@@ -12,3 +12,20 @@
 > val cube = fn : int -> int
 - cube 4;
 > val it = 64 : int
+- fun summation f n = if n = 1 then f 1 else f n + summation f (n - 1);
+> val summation = fn : (int -> int) -> int -> int
+- val newSumOfCube = summation cube;
+> val newSumOfCube = fn : int -> int
+- newSumOfCube 3;
+> val it = 36 : int
+- val sumOfSquare = summation (power 2);
+> val sumOfSquare = fn : int -> int
+- sumOfSquare 3;
+> val it = 14 : int
+- summation (power 4) 3;
+> val it = 98 : int
+- fn x => x = 1;
+> val it = fn : int -> bool
+- (fn x => x + 1) 3 * 10;
+> val it = 40 : int
+- fun f n m = (fib n) mod m = 0;
