197bb96e16802a0191049d855e4afb80b5283e58
Dement0 <ilkeer.ozgur@gmail.com>
2020-07-17 10:10:28 +0200

    Add C routines

--- improved_calculator/ast_builder.h
+++ improved_calculator/ast_builder.h
@@ -7,23 +7,24 @@ extern int yylineno; /* from lexer */
 void yyerror(char *s, ...);
 
 /* Abstract Syntax Tree nodes */
-struct abstract_syntax_tree {
+struct abstract_syntax_tree
+{
     int nodetype;
     struct abstract_syntax_tree *left;
     struct abstract_syntax_tree *right;
 };
 
-struct number_value {
+struct number_value
+{
     int nodetype;
     double number;
 };
 
 /* Abstract Syntax Tree builder */
 struct abstract_syntax_tree *new_ast(
-    int nodetype, 
+    int nodetype,
     struct abstract_syntax_tree *left,
-    struct abstract_syntax_tree *right
-    );
+    struct abstract_syntax_tree *right);
 
 struct abstract_syntax_tree *new_number_value(double value);
 
---
+++ improved_calculator/c_routines.h
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "ast_builder.h"
+
+struct abstract_syntax_tree *new_ast(
+    int nodetype,
+    struct abstract_syntax_tree *left,
+    struct abstract_syntax_tree *right
+) {
+    struct abstract_syntax_tree *ast = malloc(sizeof(struct abstract_syntax_tree));
+
+    if (!ast) {
+        yyerror("no space for ast");
+        exit(0);
+    }
+
+    ast->nodetype = nodetype;
+    ast->left = left;
+    ast->right = right;
+
+    return ast;
+}
+
+struct abstract_syntax_tree *new_number_value(double value) {
+    struct number_value *val = malloc(sizeof(struct number_value));
+
+    if(!val) {
+        yyerror("no space for number value");
+        exit(0);
+    }
+
+    val->nodetype = 'K';
+    val->number = value;
+
+    return (struct abstract_syntax_tree *)val;
+}
+
+double evaluate(struct abstract_syntax_tree *) {
+    // TODO
+}
+
+void free_tree(struct abstract_syntax_tree *) {
+    // TODO
+}
+
+void yyerror(char *s, ...) {
+    va_list arguments_print;
+    va_start(arguments_print, s);
+
+    fprintf(stderr, "%d: error: ", yylineno);
+    vprintf(stderr, s, arguments_print);
+    fprintf(stderr, "\n");
+}
+
+int main() {
+    printf("> ");
+    return yyparse();
+}
